cad92b3ead4bae9b5ab4421489f916787534dd27
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-06-05 22:17:34 +0530

    Switch to Cloudinary for file storage

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,13 +2,19 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const PDF = require("../models/PDF");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "vk-pdfs",
+        resource_type: "raw"
     }
 });
 
