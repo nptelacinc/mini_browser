b65e8d5a04985a1e4eb9678d0dec34eed5420dbc
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-06-05 12:29:20 +0530

    Refactor admin login to use environment variables

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,22 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/login", (req, res) => {
-  const { username, password } = req.body;
 
-  if (
-    username === "admin" &&
-    password === "VKWorld@2026"
-  ) {
-    return res.json({
-      success: true,
-      message: "Admin Login Success"
-    });
-  }
+    const { username, password } = req.body;
 
-  res.status(401).json({
-    success: false,
-    message: "Wrong Username or Password"
-  });
+    if (
+        username === process.env.ADMIN_USERNAME &&
+        password === process.env.ADMIN_PASSWORD
+    ) {
+        res.json({
+            success: true,
+            message: "Admin Login Successful"
+        });
+    } else {
+        res.status(401).json({
+            success: false,
+            message: "Invalid Username or Password"
+        });
+    }
+
 });
 
 module.exports = router;
