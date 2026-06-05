04169d8b61cfdde99be5f1b674fe67c8325ae377
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-06-05 11:19:57 +0530

    Implement user routes with a test endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,8 @@
-// User Routes
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({ message: "User Route Working" });
+});
+
+module.exports = router;
