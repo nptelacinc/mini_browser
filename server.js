009ae5f1c3708c3450321e5d5faf95054bc1986b
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-06-05 09:00:35 +0530

    Set up Express server with MongoDB and CORS

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("VK Connect Backend Running");
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
 });
 
-app.listen(3000, () => {
-  console.log("Server Started");
+app.get("/", (req, res) => {
+    res.send("VK Connect Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server Started on ${PORT}`);
 });
