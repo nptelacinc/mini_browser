64d5da318db2aed7d654a5fcf65dc962fc2788de
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-06-05 11:07:48 +0530

    Update server.js to serve static files and improve logging

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("MongoDB Connected");
@@ -18,12 +20,21 @@ mongoose.connect(process.env.MONGODB_URI)
     console.log(err);
 });
 
+// Static Files
+app.use(express.static(__dirname));
+
+// Home Page
 app.get("/", (req, res) => {
-    res.send("VK Connect Backend Running");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Routes
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
+
+// Server Start
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server Started on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
