aa0de04b35f6180cee9f92c54d18eff643cc029c
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-06-06 16:58:55 +0530

    Log Cloudinary credentials to console
    Add console logs for Cloudinary configuration values

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,6 +9,8 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+console.log("CLOUD NAME =", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API KEY =", process.env.CLOUDINARY_API_KEY);
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
